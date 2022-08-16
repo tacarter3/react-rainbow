db77e5b8fcd4d2417f655dda4ccf42cb7c87091e
Timothy Carter <talic2002@gmail.com>
2022-08-16 18:01:02 -0400

    Added color data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  let colors = ['violet', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
   return (
     <div className="App">
       
