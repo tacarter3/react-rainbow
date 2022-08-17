623913a540418a6cd4645603adefc009d5d86e0c
Timothy Carter <talic2002@gmail.com>
2022-08-16 20:51:38 -0400

    Added useState to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import React, { useState } from 'react'
+import ColorBlock from './ColorBlock';
 
 function App() {
-  let colors = ['violet', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+  let [colors, setColors] = useState(['violet', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock key={i} color={color}/>
+    )
+  })
+
   return (
     <div className="App">
-      
+      {colorMap}
     </div>
   );
 }
