ebb9b4aa518a477292ee29c09b7e0d1a6230bab6
Timothy Carter <talic2002@gmail.com>
2022-08-18 19:20:53 -0400

    Made some changes to the CSS and eliminated old default color values

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import ColorBlock from './ColorBlock';
 import ColorForm from './ColorForm';
 
 function App() {
-  let [colors, setColors] = useState(['violet', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+  // let [colors, setColors] = useState(['violet', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+  let [colors, setColors] = useState([])
+  
 
   const addColor = (newColor) => {
     // checks to validate color entered is a recognized CSS color
@@ -15,9 +17,10 @@ function App() {
         return colors
       }
   }
-
+  
   return (
     <div className="App">
+      <h1>Enter a CSS color value in the field below:</h1>
       <div className="container">
         {colors.map((color, i) =>
           <ColorBlock key={i} color={color} />
@@ -26,7 +29,9 @@ function App() {
       <ColorForm addColor={addColor} />
     </div>
   );
-  
+
 }
+  
+
 
 export default App;
